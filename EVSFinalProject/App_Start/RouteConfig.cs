f89faced60911e7e849958a9c6f09d1575be74ea
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-09-26 00:01:52 +0500

    Update the WebSite Routes

--- EVSFinalProject/App_Start/RouteConfig.cs
+++ EVSFinalProject/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace EVSFinalProject
@@ -16,7 +12,7 @@ namespace EVSFinalProject
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Users", action = "Login", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "index", id = UrlParameter.Optional }
             );
         }
     }
