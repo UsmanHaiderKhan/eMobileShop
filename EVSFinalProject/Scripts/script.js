883e12fb31622a908f4ffcf6ba88cb28a87ec82c
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-10-09 15:04:30 +0500

    Update the script Library

--- EVSFinalProject/Scripts/script.js
+++ EVSFinalProject/Scripts/script.js
@@ -1,23 +1,24 @@
+﻿
+document.addEventListener("DOMContentLoaded", function () {
 
-document.addEventListener("DOMContentLoaded",function(){
+    document.getElementById("slideOpen").addEventListener('click',
+        function () {
 
-    document.getElementById("slideOpen").addEventListener('click',function(){
+            document.getElementById("slideMenu").style.width = "25%";
+            document.getElementById("main").style.marginLeft = "25%";
+            document.getElementById("main").style.width = "75%";
 
-        document.getElementById("slideMenu").style.width = "25%";
-        document.getElementById("main").style.marginLeft = "25%";
-        document.getElementById("main").style.width = "75%";
-    
-    },false)
-    
-    document.getElementById("slideClose").addEventListener('click',function(){
-    
-        document.getElementById("slideMenu").style.width = "0";
-        document.getElementById("main").style.marginLeft = "0";
-        document.getElementById("main").style.width = "100%";
-    
-    },false)
+        },
+        false);
+
+    document.getElementById("slideClose").addEventListener('click',
+        function () {
+
+            document.getElementById("slideMenu").style.width = "0";
+            document.getElementById("main").style.marginLeft = "0";
+            document.getElementById("main").style.width = "100%";
+
+        },
+        false);
 
 });
-
-
-
