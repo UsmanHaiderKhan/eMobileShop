8741246585556c4cc2365414ca7f90eeb7293a99
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-10-21 22:23:52 +0500

    Update the Waether Class

--- EVSFinalProject/Models/Weather.cs
+++ EVSFinalProject/Models/Weather.cs
@@ -1,18 +1,17 @@
-﻿using System.Net;
-using System.Web.Script.Serialization;
-
-namespace EVSFinalProject.Models
+﻿namespace EVSFinalProject.Models
 {
     public class Weather
     {
-        public object GetWeatherForcast()
-        {
-            string url = "http://api.openweathermap.org/data/2.5/weather?q=Lahore&APPID=1f630ef81159132ae0101b24851e5f6e&units=imperial";
-            var client = new WebClient();
-            var content = client.DownloadString(url);
-            var serilizer = new JavaScriptSerializer();
-            var jsoncontent = serilizer.Deserialize<object>(content);
-            return jsoncontent;
-        }
+        //public object GetWeatherForcast()
+        //{
+
+
+        //    string url = "http://api.openweathermap.org/data/2.5/weather?q=Lahore&APPID=1f630ef81159132ae0101b24851e5f6e&units=imperial";
+        //    var client = new WebClient();
+        //    var content = client.DownloadString(url);
+        //    var serilizer = new JavaScriptSerializer();
+        //    var jsoncontent = serilizer.Deserialize<object>(content);
+        //    return jsoncontent;
+        //}
     }
 }
