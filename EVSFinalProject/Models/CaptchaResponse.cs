6f08ebf3a151e49dc272ad8563f93b1d72764605
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-11-02 13:25:42 +0500

    Update Added New Class for capthca code

---
+++ EVSFinalProject/Models/CaptchaResponse.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace EVSFinalProject.Models
+{
+    public class CaptchaResponse
+    {
+        [JsonProperty("success")]
+        public bool Success { get; set; }
+        [JsonProperty("eroor-code")]
+        public List<string> EroorCode { get; set; }
+    }
+}
