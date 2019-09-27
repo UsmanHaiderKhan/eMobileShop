49e461593a3f16ef39ec0484ab04dfeb1432abf8
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-09-27 18:23:42 +0500

    update the routes of the .net Application

--- EVSFinalProject/App_Start/RouteConfig.cs
+++ EVSFinalProject/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace EVSFinalProject
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "index", id = UrlParameter.Optional }
+                defaults: new { controller = "Users", action = "login", id = UrlParameter.Optional }
             );
         }
     }
