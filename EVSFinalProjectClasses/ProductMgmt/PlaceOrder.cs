5d1b9f59f6ed563bd67ad78e2409334ed3d03169
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-10-24 10:46:21 +0500

    Update added Property User_Id To Take the UserId

--- EVSFinalProjectClasses/ProductMgmt/PlaceOrder.cs
+++ EVSFinalProjectClasses/ProductMgmt/PlaceOrder.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace EVSFinalProjectClasses.ProductMgmt
 {
@@ -16,6 +12,7 @@ namespace EVSFinalProjectClasses.ProductMgmt
         public double Phone { get; set; }
 
         public List<CartItem> CartItems { get; set; }
+        public int User_Id { get; set; }
 
     }
 }
